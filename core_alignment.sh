fbbe260229d3ca37e5e230612954b20c8ff113e9
Meghan Hartwick <meghartwick@gmail.com>
2020-01-05 15:25:28 -0500

    Update core_alignment.sh

--- core_alignment.sh
+++ core_alignment.sh
@@ -1,18 +1,7 @@
+#run a phylogenetic and recombination analysis of the core genome 
 #!/bin/sh
 
-run_gubbins.py --threads 16 *core_gene_alignment.aln
 raxmlHPC-PTHREADS-AVX -s core_gene_alignment.fasta -n raxml_GBE_core.out -m GTRCAT -f a -x 123 -T 16 -N autoMRE -p 456
-
-#module purge
-#module load anaconda/colsa
-
-#source activate  snp-sites
-#snp-sites -v core_gene_alignment.aln
-
-#source deactivate
-#module purge
-#module load linuxbrew/colsa
-
 cp RAxML_bestTree.raxml_GBE_core.out RAxML_bestTree.raxml_GBE_core.nwk
 ClonalFrameML RAxML_bestTree.raxml_GBE_core.nwk core_gene_alignment.fasta core_genome_recomb -embranch
 
